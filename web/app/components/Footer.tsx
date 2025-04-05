80449557fdbb6ec1b48517a3fba23821adc984fa
HemangVora <hemangvora2@gmail.com>
2025-04-06 00:40:30 +0800

    Footer fix

---
+++ web/app/components/Footer.tsx
@@ -0,0 +1,46 @@
+import { motion } from "framer-motion";
+function Footer() {
+  return (
+    <footer className="py-6 mt-12 border-t border-gray-800 text-center items-center text-[#fff]">
+      <div className="container mx-auto">
+        <div
+          className="flex flex-col items-center justify-center text-center w-full"
+          style={{ fontFamily: "var(--font-permanent-marker)" }}
+        >
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+            className="flex items-center font-Pretendard font-[800] gap-2 text-[#FFFFFF] text-[28px] md:text-[24px]     text-left"
+          >
+            <img
+              className="h-[26px] w-auto mr-2"
+              src={"/assets/logo.png"}
+              alt=""
+            />
+            <span style={{ fontFamily: "var(--font-permanent-marker)" }}>
+              brain_rot
+            </span>
+            {"   "}
+            <span className="font-normal">
+              is place where memes come to life!
+            </span>
+            <span className="font-normal  ">
+              Made with <span className="text-red-500">❤️</span> by{" "}
+              <a
+                href="https://x.com/corotvoid"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-[#2AD8D3] hover:underline"
+              >
+                COROT
+              </a>
+            </span>
+          </motion.div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- web/app/working/page.tsx
+++ web/app/working/page.tsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import styles from "../../styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import PrimaryButton from "../components/primaryButton";
+import Footer from "../components/Footer";
 
 const Working = () => {
   return (
@@ -20,31 +21,11 @@ const Working = () => {
                 initial={{ opacity: 0, x: -20 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.6 }}
-                className="font-Pretendard text-[#FFFFFF] text-[48px] md:text-[68px] font-bold text-left"
+                style={{ fontFamily: "var(--font-permanent-marker)" }}
+                className="font-Pretendard text-[#FFFFFF] text-[48px]  md:text-[68px] font-bold text-left"
               >
-                How it works
+                How it works!
               </motion.h1>
-            </div>
-            <div className="flex flex-col items-start justify-start text-start w-full">
-              <motion.div
-                initial={{ opacity: 0, x: -20 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.6, delay: 0.2 }}
-                className="flex items-center font-Pretendard font-[800] gap-2 text-[#FFFFFF] text-[28px] md:text-[24px] max-w-2xl   text-left"
-              >
-                <img
-                  className="h-[26px] w-auto mr-2"
-                  src={"/assets/logo.png"}
-                  alt=""
-                />
-                <span style={{ fontFamily: "var(--font-permanent-marker)" }}>
-                  brain_rot
-                </span>
-                {"   "}
-                <span className="font-normal">
-                  is place where memes come to life.
-                </span>
-              </motion.div>
             </div>
             <div
               className="flex flex-col items-start justify-start text-start w-full"
@@ -81,7 +62,7 @@ const Working = () => {
                   the prize pool's liquidity
                 </motion.div>
               </div>
-            </div>
+            </div>{" "}
           </div>
           <div
             className="flex flex-col items-start justify-start text-start w-full"
@@ -101,24 +82,7 @@ const Working = () => {
         </div>
       </main>
 
-      <footer className="py-6 mt-12 border-t border-gray-800 text-center text-[#fff]">
-        <div className="container mx-auto">
-          <p className="flex items-center justify-center gap-2">
-            Made with <span className="text-red-500">❤️</span> by
-            <a
-              href="https://github.com/brain-rot"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-[#2AD8D3] hover:underline"
-            >
-              brain_rot
-            </a>
-          </p>
-          <p className="mt-2 text-sm">
-            © {new Date().getFullYear()} All rights reserved
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
